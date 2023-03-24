9fa9eb4d2c7f3a9bc2fe61df8799c323ecc1a9d1
Carlo de Villiers <64757000+carlodevil@users.noreply.github.com>
2023-03-24 13:49:35 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  purge: [
+  content: [
     "./assets/**/*.{css}",
     "./components/*.{vue,js}",
     "./components/**/*.{vue,js}",
@@ -12,22 +12,6 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   content: [],
-  theme: {
-    extend: {
-      green: {
-        DEFAULT: "#29A69F",
-        50: "#E9FAF9",
-        100: "#D0F3F2",
-        200: "#9FE7E3",
-        300: "#6EDBD5",
-        400: "#3DCFC7",
-        500: "#29A69F",
-        600: "#20817C",
-        700: "#175C58",
-        800: "#0E3835",
-        900: "#051312",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
+  plugins: [require("@tailwindcss/aspect-ratio")],
 }
